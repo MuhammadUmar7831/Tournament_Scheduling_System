8153f0bdfb114c1fe730722b8f4434fd5c758e9b
Muhammad Umar <mu8494759@gmail.com>
2024-05-23 21:08:16 +0500

    atlas env added

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // my atlas uri
-const uri = 'mongodb://127.0.0.1:27017/tournaments'
+// const uri = 'mongodb://127.0.0.1:27017/tournaments'
+const uri = process.env.uri
 
 var rtn = false;
 // establsihing connection with inotebook atlas mongodb
 const connectToDatabase = async () => {
     await mongoose.connect(uri)
         .then(() => {console.log('connetction to databse successfull......');rtn = true})
-        .catch(() => console.log("failed to connect to database :(")); // potential hit on internet connection loss
+        .catch((error) => console.log("failed to connect to database :(")); // potential hit on internet connection loss
     return rtn;
 }
 
