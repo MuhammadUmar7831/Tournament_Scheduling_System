a4ca2f8d88a22c1a00eae3b1436b9c27e9b4ceb3
Muhammad Umar <mu8494759@gmail.com>
2024-05-23 21:28:39 +0500

    wellcome routes fix

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/getSchedules", async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send("Successfull");
 });
 
